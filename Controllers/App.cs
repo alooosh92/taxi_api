aad52c84e5544d324bb1104e32b9ecd895f9ab6d
alaa baaj <the.lost.admiral@gmail.com>
2023-11-27 13:00:24 +0300

    a

--- Controllers/App.cs
+++ Controllers/App.cs
@@ -240,7 +240,7 @@ namespace taxi_api.Controllers
         }
         [HttpGet]
         [Route("GetAllTrip")]
-        [Authorize(AuthenticationSchemes = "Bearer", Roles = "Driver,Admin")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = "User")]
         public async Task<ActionResult<List<ShowTripVM>>> GetAllTrip()
         {
             try
